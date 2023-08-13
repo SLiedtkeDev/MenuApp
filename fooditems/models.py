2dd9173288e44fd6158fff1b3a338f68e9d10324
SLiedtkeDev <stephanliedtke92@gmail.com>
2023-08-13 11:09:47 -0600

    detail view and img urls

--- fooditems/models.py
+++ fooditems/models.py
@@ -10,3 +10,5 @@ class item(models.Model):
     item_name = models.CharField(max_length=200)
     item_description = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(
+        max_length=500, default="https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png")
