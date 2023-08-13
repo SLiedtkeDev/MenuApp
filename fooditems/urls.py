0189fa9186c7de8a11c53de819f909f5d2483a2e
SLiedtkeDev <stephanliedtke92@gmail.com>
2023-08-13 08:18:48 -0600

    basic html and css

--- fooditems/urls.py
+++ fooditems/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'fooditems'
 urlpatterns = [
     path("", views.index, name="index"),
-    path("item/", views.items, name="item")
+    path("item/", views.items, name="item"),
+    path('<int:item_id>/', views.detail, name='detail'),
 ]
--- fooditems/views.py
+++ fooditems/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import item
+from django.template import loader
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("Hello World")
+    item_list = item.objects.all()
+    context = {
+        'item_list': item_list,
+    }
+    return render(request, 'fooditems/index.html', context)
+
 
 def items(request):
     return HttpResponse("<h1>This is an item</h1>")
 
+
+def detail(request, item_id):
+    current_item = item.objects.get(pk=item_id)
+    context = {
+        'item': current_item,
+    }
+    return render(request, 'fooditems/detail.html', context)
