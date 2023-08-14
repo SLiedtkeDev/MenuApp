6c64cea208058adc3ff1adf7fa5787eb155274f8
SLiedtkeDev <stephanliedtke92@gmail.com>
2023-08-13 19:35:12 -0600

    adding delete and upate functionality

--- fooditems/urls.py
+++ fooditems/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("item/", views.items, name="item"),
     path('<int:item_id>/', views.detail, name='detail'),
+    path('add', views.add_item, name='add_item'),
+    path('update/<int:id>/', views.update_item, name='update_item'),
+    path('delete/<int:id>/', views.delete_item, name='delete_item')
 ]
--- fooditems/views.py
+++ fooditems/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import item
 from django.template import loader
+from .forms import ItemForm
 
 # Create your views here.
 
@@ -24,3 +25,28 @@ def detail(request, item_id):
         'item': current_item,
     }
     return render(request, 'fooditems/detail.html', context)
+
+
+def add_item(request):
+    form = ItemForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('fooditems:index')
+    return render(request, 'fooditems/item-form.html', {'form': form})
+
+
+def update_item(request, id):
+    current_item = item.objects.get(id=id)
+    form = ItemForm(request.POST or None, instance=current_item)
+    if form.is_valid():
+        form.save()
+        return redirect('fooditems:index')
+    return render(request, 'fooditems/item-form.html', {'form': form, 'item': current_item})
+
+
+def delete_item(request, id):
+    current_item = item.objects.get(id=id)
+    if request.method == "POST":
+        item.delete()
+        return redirect('fooditems:index')
+    return render(request, 'fooditems/item-delete.html', {'item': current_item})
